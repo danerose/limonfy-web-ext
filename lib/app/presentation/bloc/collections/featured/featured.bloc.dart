3c75e01aeacf91a22dd6647d0b732dcaceb8ca5a
Danerose <valencia3698@gmail.com>
2023-07-24 21:27:55 -0500

    Final Tocuher

--- lib/app/presentation/bloc/collections/featured/featured.bloc.dart
+++ lib/app/presentation/bloc/collections/featured/featured.bloc.dart
@@ -36,7 +36,6 @@ class FeaturedCollectionBloc extends Bloc<FeatCollecEvent, FeatCollecState> {
       (CustomException l) => premium = false,
       (SubscriptionResponse r) => premium = r.subscription.premium,
     );
-    premium = true;
     if (premium) {
       final res = await _getFeatureCollectionUsecase.execute(
         refresh: event.refresh,

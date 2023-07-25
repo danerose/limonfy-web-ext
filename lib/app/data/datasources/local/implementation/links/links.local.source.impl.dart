28c4b2e8fb7a86b3da8f4d3b10b46c4c27be3aed
Danerose <valencia3698@gmail.com>
2023-07-24 21:51:37 -0500

    aDD URL BANNER

--- lib/app/data/datasources/local/implementation/links/links.local.source.impl.dart
+++ lib/app/data/datasources/local/implementation/links/links.local.source.impl.dart
@@ -2,10 +2,27 @@ import 'dart:convert';
 
 import 'package:limonfy/app/data/models/link/link.model.dart';
 import 'package:limonfy/app/data/datasources/local/interface/links/links.local.source.dart';
+import 'package:limonfy/core/constants/limonfy.constants.dart';
 
 class LinksLocalSourceImpl implements LinksLocalSource {
   @override
   Future<LinkModel> convertFromScript({required String link}) async {
-    return LinkModel.fromJson(jsonDecode(link));
+    final l = LinkModel.fromJson(jsonDecode(link));
+    const name = LimonfyConstants.limonfy;
+    const icon = LimonfyConstants.urlIcon;
+    return LinkModel(
+      id: l.id,
+      link: l.link,
+      title: l.title.isEmpty ? name : l.title,
+      description: l.description.isEmpty ? name : l.description,
+      sourceName: l.sourceName.isEmpty ? name : l.sourceName,
+      sourceImageUrl: l.sourceImageUrl.isEmpty ? icon : l.sourceImageUrl,
+      imageUrl: l.imageUrl.isEmpty ? LimonfyConstants.urlBanner : l.imageUrl,
+      private: l.private,
+      linkSaveMethodId: l.linkSaveMethodId,
+      userAccountId: l.userAccountId,
+      updatedAt: l.updatedAt,
+      createdAt: l.createdAt,
+    );
   }
 }
